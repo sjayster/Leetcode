4296ad4d99906ffce68bd813766da6e6cff0e084
srijay <srijayanth.vs@gmail.com>
2016-09-01 12:39:58 -0700

    Solution for #231 - Power of two

---
+++ power-of-two.py
@@ -0,0 +1,16 @@
+"""
+Power of 2:
+    if x and (x-1) == 0, then x is a power of 2 (except when x == 0).
+    example: x = 8
+    8 & 7 = 1000 & 0111 = 0
+"""
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        return n & (n-1) == 0
