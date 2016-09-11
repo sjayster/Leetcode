183f2fae36d2f7bed41bd5c4bb2f1043050f98de
srijay <srijayanth.vs@gmail.com>
2016-09-11 00:09:35 -0700

    Solution for #342 Power of four

--- power-of-four.py
+++ power-of-four.py
@@ -1,4 +1,9 @@
 """
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example:
+Given num = 16, return true. Given num = 5, return false.
+
 Same as power of 3
 Power of 4 numbers would always have a reminder of one. Keep doing that over a loop while n > 1 and n/=4
 Finally, if n == 1 return True, else False
