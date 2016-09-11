69ce881c60fb397c345d1de3f4f4cee89d30386d
srijay <srijayanth.vs@gmail.com>
2016-09-11 00:03:21 -0700

    Solution for #237 Delete Node in a Linked List

--- delete-a-node-in-a-linked-list.py
+++ delete-a-node-in-a-linked-list.py
@@ -1,4 +1,9 @@
 """
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+
+Solution:
 Since we only know that node, we change the value and the next of the given node to its next value.
 """
 
