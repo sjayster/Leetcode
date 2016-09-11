ac627f67e4da6a18029b8ea347aca83b36ab9872
srijay <srijayanth.vs@gmail.com>
2016-09-11 00:06:45 -0700

    Solution for #350 Intersection of Two Arrays II

--- intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii.py
@@ -1,4 +1,13 @@
 """
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+
+Note:
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+
 Solution: Use a hash map/dictionary
 
 Since we have no restriction like the number must be seen only once, we can skip that step.
