be6c2a3badc074a2215347f73f95f441d12811df
srijay <srijayanth.vs@gmail.com>
2016-09-11 00:12:56 -0700

    Solution for #1 Two sum

--- two-sum.py
+++ two-sum.py
@@ -1,5 +1,14 @@
 """
-Asked in Amazon
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
 Iterate over list and add the elements to the dictionary with the index as the value.
 Iterate over the list again and see if an element (target - nums[index]) exists in the dictionary.
 See if the value is less (or greater) than the current index (to avoid duplication, like [1,3,3,3])
