c0197aed53787c2b82c0d748503b3006e1a1ab64
srijay <srijayanth.vs@gmail.com>
2016-09-11 00:10:10 -0700

    Solution for #231 Power of two

--- power-of-two.py
+++ power-of-two.py
@@ -1,5 +1,7 @@
 """
-Power of 2:
+Given an integer, write a function to determine if it is a power of two.
+
+Power of 2: Bit manipulation
     if x and (x-1) == 0, then x is a power of 2 (except when x == 0).
     example: x = 8
     8 & 7 = 1000 & 0111 = 0
