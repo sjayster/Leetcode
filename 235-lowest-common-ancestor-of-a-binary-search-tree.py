099d1b62ba3a55bd4119b6d6708b338e148070fb
sjayster <srijayanth.vs@gmail.com>
2017-04-29 23:46:29 -0700

    235-lowest-common-ancestor-of-a-binary-search-tree.py

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,35 @@
+"""
+Solution:
+
+The LCA will be the node whose val will be greater than p and lesser than q.
+1. If p.val < root.val and q.val < root.val, then we traverse left
+2. If p.val > root.val and q.val > root.val, we traverse right
+3. else we return the root 
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root or not p or not q:
+            return None
+
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
